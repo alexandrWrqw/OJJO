04faa3ddf5978fd86a0504f7617299aba19d434b
AlexandrWrqw <139682068+alexandrWrqw@users.noreply.github.com>
2023-09-20 17:46:14 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Nav mobile
 const navBtn = document.querySelector('.nav-icon-btn');
 const navIcon = document.querySelector('.nav-icon');
 const nav = document.querySelector('.nav');
@@ -7,3 +8,57 @@ navBtn.onclick = function () {
     nav.classList.toggle('nav--mobile');
     document.body.classList.toggle('no-scroll');
 }
+// Custom select
+document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+    const dropDownBtn = dropDownWrapper.querySelector('.sorting__button');
+    const dropDownList = dropDownWrapper.querySelector('.sorting__list');
+    const dropDownListItems = dropDownList.querySelectorAll('.sorting__list-item');
+    const dropDownInput = dropDownWrapper.querySelector('.sorting__input-hidden');
+
+
+    dropDownBtn.addEventListener('click', function () {
+        dropDownList.classList.toggle('sorting__list--visible');
+        this.classList.add('sorting__button--active');
+    });
+
+    dropDownListItems.forEach(function (listItem) {
+        listItem.addEventListener('click', function (event) {
+            event.stopPropagation();
+            dropDownBtn.innerText = this.innerText;
+            dropDownBtn.classList.remove('sorting__button--active');
+            dropDownInput.value = this.dataset.value;
+            dropDownList.classList.remove('sorting__list--visible');
+        });
+    });
+
+    document.addEventListener('click', function (event) {
+        if (event.target !== dropDownBtn) {
+            dropDownList.classList.remove('sorting__list--visible');
+            dropDownBtn.classList.remove('sorting__button--active');
+        };
+    });
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Tab' || event.key === 'Escape') {
+            dropDownList.classList.remove('sorting__list--visible');
+            dropDownBtn.classList.remove('sorting__button--active');
+        };
+    });
+});
+// More-btn catalog block
+const catalogCards = document.querySelector('.catalog__cards');
+const moreBtn = document.querySelector('.more-btn');
+
+moreBtn.addEventListener('click', function() {
+    catalogCards.classList.toggle('catalog__cards--active');
+    this.textContent = this.textContent === 'Покажите еще' ? 'Скрыть' : 'Покажите еще';
+});
+// Read-btn feature block
+const readBtn = document.querySelector('.read-btn');
+const featureInfo = document.querySelector('.feature__info');
+
+readBtn.addEventListener('click', function() {
+    featureInfo.classList.toggle('feature__info--active');
+    this.textContent = this.textContent === 'Читать полностью' ? 'Скрыть' : 'Читать полностью';
+});
+
