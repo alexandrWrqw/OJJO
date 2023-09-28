72c5faa525a8835a5899c1a05c7af5578bdc2f28
AlexandrWrqw <139682068+alexandrWrqw@users.noreply.github.com>
2023-09-28 17:24:59 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -73,37 +73,42 @@ document.querySelectorAll('.feature__container').forEach(function (featureWrappe
     });
 });
 // Swiper product block
-new Swiper ('.swiper-container', {
+let slider = new Swiper ('.swiper', {
     navigation: {
         nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+    
+    zoom: {
+        maxRatio: 5,
+        minRatio: 1,
     },
 });
-
+// Click image => open swiper
 const body = document.querySelector('body')
-const content = document.querySelector('.swiper');
+const content = document.querySelector('.product__swiper');
+const openBtns = document.querySelectorAll('.image-btn')
 const closeBtn = document.querySelector('.close-btn');
+
+openBtns.forEach(function (imageBtn) {
+    imageBtn.addEventListener('click', () => {
+        slider.slideTo(0, 800);
+        content.classList.remove('none');
+        body.classList.add('no-scroll');
+    })
+})
 
 closeBtn.addEventListener('click', function () {
     content.classList.add('none');
-    body.classList.remove('no-scroll')
+    body.classList.remove('no-scroll');
 });
-
-// const openBtn = document.querySelector('.image-btn');
-// openBtn.addEventListener('click', function () {
-//     content.classList.remove('none');
-//     body.classList.add('no-scroll');
-// });
-
-// document.querySelectorAll('.image-btn').forEach(function(open) {
-
-//     closeBtn.addEventListener('click', function () {
-//         content.classList.add('none');
-//         body.classList.remove('no-scroll')
-//     });
-
-//     .addEventListener('click', function () {
-//         content.classList.remove('none');
-//         body.classList.add('no-scroll');
-//     });
-// })
